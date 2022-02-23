a3b032b107204178fbf0f740176e486c2cc2bd4f
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2022-02-23 10:14:45 +0100

    ui: remove logs page
    Logs page will be inside the core UI

--- ui/src/components/AppSideMenuContent.vue
+++ ui/src/components/AppSideMenuContent.vue
@@ -25,13 +25,6 @@
         <span>{{ $t("settings.title") }}</span>
       </cv-side-nav-link>
       <cv-side-nav-link
-        @click="goToPage('logs')"
-        :class="{ 'current-page': isLinkActive('logs') }"
-      >
-        <template v-slot:nav-icon><Catalog20 /></template>
-        <span>{{ $t("logs.title") }}</span>
-      </cv-side-nav-link>
-      <cv-side-nav-link
         @click="goToPage('about')"
         :class="{ 'current-page': isLinkActive('about') }"
       >
@@ -44,7 +37,6 @@
 
 <script>
 import Settings20 from "@carbon/icons-vue/es/settings/20";
-import Catalog20 from "@carbon/icons-vue/es/catalog/20";
 import Information20 from "@carbon/icons-vue/es/information/20";
 import Activity20 from "@carbon/icons-vue/es/activity/20";
 import { mapState } from "vuex";
@@ -53,7 +45,6 @@ import { QueryParamService } from "@nethserver/ns8-ui-lib";
 export default {
   name: "AppSideMenuContent",
   components: {
-    Catalog20,
     Settings20,
     Information20,
     Activity20,
